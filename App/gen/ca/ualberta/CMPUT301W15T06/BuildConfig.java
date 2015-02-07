996193e0ea0b6877a67cea6bdb7553c6edbf61a7
jiafengwu0301 <jack.wu1224@gmail.com>
2015-02-07 14:38:42 -0700

    create project

---
+++ App/gen/ca/ualberta/CMPUT301W15T06/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ca.ualberta.CMPUT301W15T06;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ AppTest/gen/ca/ualberta/CMPUT301W15T06/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ca.ualberta.CMPUT301W15T06.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
