fd6a3ad81043a39d1f1f9f56cb574bbc461a1e50
agileshaw <agileshaw@users.noreply.github.com>
2015-02-10 15:31:20 -0700

    Add two test file(unmodified)

---
+++ AppTest/src/ca/ualberta/CMPUT301W15T06/test/ExpanseClaimOverallTest.java
@@ -0,0 +1,15 @@
+package ca.ualberta.CMPUT301W15T06.test;
+
+import junit.framework.TestCase;
+
+public class ExpanseClaimOverallTest extends TestCase {
+
+	public ExpanseClaimOverallTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
---
+++ AppTest/src/ca/ualberta/CMPUT301W15T06/test/ExpenseClaimListingTest.java
@@ -0,0 +1,15 @@
+package ca.ualberta.CMPUT301W15T06.test;
+
+import junit.framework.TestCase;
+
+public class ExpenseClaimListingTest extends TestCase {
+
+	public ExpenseClaimListingTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
