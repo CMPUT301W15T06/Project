3dbfa0d1a5b010b80b0699d58c9070acf0137e9f
Nidedaye <jonathan.nijingqiao@gmail.com>
2015-02-12 15:48:09 -0700

    add part UI

--- App/gen/ca/ualberta/CMPUT301W15T06/R.java
+++ App/gen/ca/ualberta/CMPUT301W15T06/R.java
@@ -110,7 +110,14 @@ public final class R {
         public static final int activity_main=0x7f030007;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int claiment_add_claim=0x7f070000;
+        public static final int claiment_add_item=0x7f070001;
+        public static final int claiment_claim_list=0x7f070002;
+        public static final int claiment_edit_claim=0x7f070003;
+        public static final int claiment_edit_item=0x7f070004;
+        public static final int claiment_item_list=0x7f070005;
+        public static final int item=0x7f070006;
+        public static final int main=0x7f070007;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
