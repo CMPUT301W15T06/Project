d1313f8779439ba24fba2ef29d7f2aba31dd38b4
agileshaw <agileshaw@users.noreply.github.com>
2015-02-12 16:05:20 -0700

    add some test cases

--- AppTest/src/ca/ualberta/CMPUT301W15T06/test/ClaimListTest.java
+++ AppTest/src/ca/ualberta/CMPUT301W15T06/test/ClaimListTest.java
@@ -1,5 +1,6 @@
 package ca.ualberta.CMPUT301W15T06.test;
 
+import ca.ualberta.CMPUT301W15T06.Claim;
 import ca.ualberta.CMPUT301W15T06.ClaimList;
 import android.test.ActivityInstrumentationTestCase2;
 
@@ -13,4 +14,22 @@ public class ClaimListTest extends ActivityInstrumentationTestCase2<ClaimList> {
 		super.setUp();
 	}
 
+	//US01.04.01 test if a new claim can be successfully added
+	public void testAddClaim()
+	{
+		String newClaim = "new claim";
+		Claim claim = new Claim(newClaim);
+		assertEquals("new claim is not added", newClaim.equals(claim.getClaim()));	
+	}
+	
+	// test if the claim can be saved and push online
+	public void testPushOnline(){
+		String name = "Travel";
+		Claim claim = new Claim(name);
+		ClaimList claimList = new ClaimList();
+		claimList.addClaim(claim);
+		claimList.pushOnline();
+		assertTrue("Push Online unsuccessfully",claim.equals(claimList.pullOnline()));
+	}
+	
 }
--- AppTest/src/ca/ualberta/CMPUT301W15T06/test/ClaimTest.java
+++ AppTest/src/ca/ualberta/CMPUT301W15T06/test/ClaimTest.java
@@ -1,6 +1,7 @@
 package ca.ualberta.CMPUT301W15T06.test;
 
 import ca.ualberta.CMPUT301W15T06.Claim;
+import ca.ualberta.CMPUT301W15T06.Item;
 import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
 
@@ -14,29 +15,24 @@ public class ClaimTest extends ActivityInstrumentationTestCase2<Claim> {
 		super.setUp();
 	}
 	
-	public void testName()
+	// US01.01.01 test if the system records the claimant information correctly
+	public void testRecordInfo()
 	{
+		// add name
 		String name = "Name";
 		Claim claimantName = new Claim(name);
 		Claim.setName(name);
 		assertNotSame("The Name is not equal", name, claimantName.getName());
-	}
-	
-	public void testBeginDate()
-	{
+		// add starting date
 		String beginDate = "2015-02-18";
 		Claim BeginDate = new Claim(beginDate);
 		Claim.setBeginDate(beginDate);
 		assertEquals("The begin date is", beginDate, BeginDate.getBeginDate());
-	}
-
-	public void testEndDate()
-	{
+		// add ending date
 		String endDate = "2015-03-08";
 		Claim EndDate = new Claim(endDate);
 		Claim.setEndDate(endDate);
 		assertEquals("The begin date is", endDate, EndDate.getBeginDate());
 	}
-	
 
 }
