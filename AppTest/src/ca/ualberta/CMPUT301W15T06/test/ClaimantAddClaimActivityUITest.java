907f15e4e74ea24be9223eb6ba64e78921213722
agileshaw <agileshaw@users.noreply.github.com>
2015-02-23 13:50:02 -0700

    fullfill two added UI test files

--- AppTest/src/ca/ualberta/CMPUT301W15T06/test/ClaimantAddClaimActivityUITest.java
+++ AppTest/src/ca/ualberta/CMPUT301W15T06/test/ClaimantAddClaimActivityUITest.java
@@ -1,16 +1,23 @@
 package ca.ualberta.CMPUT301W15T06.test;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
-import org.junit.Test;
 
 import ca.ualberta.CMPUT301W15T06.ClaimantAddClaimActivity;
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ClaimantAddClaimActivityUITest extends
 		ActivityInstrumentationTestCase2<ClaimantAddClaimActivity> {
 
+	Instrumentation instrumentation;
+	Activity activity;
+	EditText claimant_name;
+	EditText claimant_starting_date;
+	EditText claimant_ending_date;
+	
 	public ClaimantAddClaimActivityUITest() {
 		super(ClaimantAddClaimActivity.class);
 	}
@@ -18,11 +25,34 @@ public class ClaimantAddClaimActivityUITest extends
 	@Before
 	protected void setUp() throws Exception {
 		super.setUp();
+		instrumentation = getInstrumentation();
+		activity = getActivity();
+		claimant_name = ((EditText) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.createClaimNameEditText));
+		claimant_starting_date = ((EditText) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.createClaimStartingDateEditText));
+		claimant_ending_date = ((EditText) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.createClaimEndDateEditText));
 	}
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	// fill blank
+	@SuppressWarnings("unused")
+	private void testAddButton(String claimantName, String claimantStartingDate, String itemEndingDate) {
+		assertNotNull(activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.FinishItemButton));
+		
+		claimant_name.setText(claimantName);
+		claimant_starting_date.setText(claimantStartingDate);
+		claimant_ending_date.setText(itemEndingDate);
+		
+		((Button) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.FinishItemButton)).performClick();
 	}
 
+	//test if add a new claim to list
+	public void testClickAddButton(String claimantName, String claimantStartingDate, String itemEndingDate) {
+		assertNotNull(activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.FinishItemButton));
+		
+		claimant_name.setText(claimantName);
+		claimant_starting_date.setText(claimantStartingDate);
+		claimant_ending_date.setText(itemEndingDate);
+		
+		((Button) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.FinishItemButton)).performClick();
+	    assertNotNull(activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.claimListView));	    
+	}
 }
--- AppTest/src/ca/ualberta/CMPUT301W15T06/test/ClaimantDestinationReasonActivityUITest.java
+++ AppTest/src/ca/ualberta/CMPUT301W15T06/test/ClaimantDestinationReasonActivityUITest.java
@@ -1,16 +1,23 @@
 package ca.ualberta.CMPUT301W15T06.test;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
-import org.junit.Test;
 
 import ca.ualberta.CMPUT301W15T06.ClaimantDestinationReasonActivity;
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.EditText;
 
+@SuppressWarnings("unused")
 public class ClaimantDestinationReasonActivityUITest extends
 		ActivityInstrumentationTestCase2<ClaimantDestinationReasonActivity> {
 
+	Instrumentation instrumentation;
+	Activity activity;
+	EditText claimant_des;
+	EditText claimant_reason;
+	
 	public ClaimantDestinationReasonActivityUITest() {
 		super(ClaimantDestinationReasonActivity.class);
 	}
@@ -18,11 +25,30 @@ public class ClaimantDestinationReasonActivityUITest extends
 	@Before
 	protected void setUp() throws Exception {
 		super.setUp();
+		instrumentation = getInstrumentation();
+		activity = getActivity();
+		claimant_des = ((EditText) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.DestinationEditText));
+		claimant_reason = ((EditText) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.ReasonEditText));
 	}
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	// fill blank
+	private void testAddButton(String claimantDes, String claimantReason) {
+		assertNotNull(activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.FinishItemButton));
+		
+		claimant_des.setText(claimantDes);
+		claimant_reason.setText(claimantReason);
+		
+		((Button) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.FinishItemButton)).performClick();
 	}
+
+	//test if add a new claim to list
+	public void testClickAddButton(String claimantDes, String claimantReason) {
+		assertNotNull(activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.FinishItemButton));
+		
+		claimant_des.setText(claimantDes);
+		claimant_reason.setText(claimantReason);
+		
+		((Button) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.FinishItemButton)).performClick();
+	    assertNotNull(activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.claimListView));	 
 
 }
