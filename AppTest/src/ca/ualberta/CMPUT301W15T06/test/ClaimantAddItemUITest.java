bf79ba74dd997f3a2dde198173d1305fdc314ad9
han95 <han8@ualberta.ca>
2015-03-02 14:30:27 -0700

    modify ClaimantAddItemUITest, add spinner test and part of button test

--- AppTest/src/ca/ualberta/CMPUT301W15T06/test/ClaimantAddItemUITest.java
+++ AppTest/src/ca/ualberta/CMPUT301W15T06/test/ClaimantAddItemUITest.java
@@ -4,6 +4,12 @@ import ca.ualberta.CMPUT301W15T06.ClaimantAddItemActivity;
 import android.app.Activity;
 import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.test.ViewAsserts;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -11,7 +17,19 @@ import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 import ca.ualberta.CMPUT301W15T06.R;
 
+/*
+ * Reference:
+ * http://developer.android.com/tools/testing/activity_test.html
+ */
 
+/*
+ * This is UI test class for ClaimantAddItemActivity (US04.01.01)
+ * 
+ * This test contains the tests of: 
+ * 		1. whether the text field and button are shown on screen
+ * 		2. whether the texts shown on screen are correct
+ * 		3. whether clicking button triggers saving inputs and starting new activity
+ */
 public class ClaimantAddItemUITest extends
 		ActivityInstrumentationTestCase2<ClaimantAddItemActivity>
 {
@@ -23,49 +41,201 @@ public class ClaimantAddItemUITest extends
 	EditText item_des;
 	EditText item_amount;
 	Spinner item_currency;
+	Button finish_button;
+	
+	private SpinnerAdapter cate;
+	private SpinnerAdapter curr;
+	
+	public static final int INITIAL_POSITION = 0; 
+	public static final int ADAPTER_COUNT = 9;
+	public static final int TEST_POSITION = 5;
+	
+	private String mSelection;
+	private int mPos;
+	
 
+	// test case constructor
 	public ClaimantAddItemUITest()
 	{
-
 		super(ClaimantAddItemActivity.class);
-	}
+	} 
 
+	// set up method
 	protected void setUp() throws Exception
 	{
 		super.setUp();
 		instrumentation = getInstrumentation();
+		
+		// get a reference to the activity under test. If the activity is not starts, start a new activity
 		activity = getActivity();
-		item_date = ((EditText) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.createItemDateEditText));
-		item_category = ((Spinner) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.createItemCategorySpinner));
-		item_des = ((EditText) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.createItemDescriptionEditText));
-		item_amount = ((EditText) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.createItemAmountEditText));
-		item_currency = ((Spinner) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.createCurrencySpinner));	
-		// http://developer.android.com/tools/testing/activity_test.html 2015-02-13
+		
+		// get text fields and form widgets
+		item_date = (EditText) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.createItemDateEditText);
+		item_category = (Spinner) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.createItemCategorySpinner);
+		item_des = (EditText) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.createItemDescriptionEditText);
+		item_amount = (EditText) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.createItemAmountEditText);
+		item_currency = (Spinner) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.createCurrencySpinner);	
+		finish_button = (Button) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.FinishItemButton);
 	    cate = item_category.getAdapter();
-	}
+	    curr = item_currency.getAdapter();
+	    
+	    /* This turns off touch mode in the device or emulator. 
+	     * If any of your test methods send key events to the application, 
+	     * you must turn off touch mode before you start any activities; 
+	     * otherwise, the call is ignored.
+	     */
+	    setActivityInitialTouchMode(false);
+
+	} // end of setUp method
 	
-	// http://developer.android.com/tools/testing/activity_test.html 2015-01-13
-	private ClaimantAddItemActivity mActivity;
-	private Spinner i_cate;
-	private SpinnerAdapter cate;
-	public static final int INITIAL_POSITION = 0;
+	/*
+	 * This part is spinner testing
+	 * spinner id: createItemCategorySpinner
+	 * 			   createCurrencySpinner
+	 */
 	
-	public void testSpinnerUI() {
-		mActivity.runOnUiThread(
+	// test pre condition
+	public void testPreConditions() {
+		// listeners for two spinners are not null
+		assertTrue(item_category.getOnItemSelectedListener() != null);
+		assertTrue(item_currency.getOnItemSelectedListener() != null);
+		
+		// item_category has an adapter
+		assertTrue(cate != null);
+		assertEquals(cate.getCount(),ADAPTER_COUNT);
+		
+		// item_currency has an adapter
+		assertTrue(curr != null);
+		assertEquals(curr.getCount(),ADAPTER_COUNT);
+		
+	} 
+	
+	
+	// test category spinner
+	public void testCategorySpinnerUI() {
+		
+		// run activity
+		activity.runOnUiThread(
 			new Runnable() {
 		        public void run() {
-		          i_cate.requestFocus();
-		          i_cate.setSelection(INITIAL_POSITION);
+		        	item_category.requestFocus();
+		        	item_category.setSelection(INITIAL_POSITION);
 		        }
 			});
-	}
+		
+		// make a selection
+	    this.sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
+	    for (int i = 1; i <= TEST_POSITION; i++) {
+	      this.sendKeys(KeyEvent.KEYCODE_DPAD_DOWN);
+	    } 
+	    
+	    // send key event
+	    this.sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
+	    
+	    // check the result
+	    mPos = item_category.getSelectedItemPosition();
+	    mSelection = (String)item_category.getItemAtPosition(mPos);
+	    
+	    // get the spinner result text view
+	    TextView resultView = (TextView) activity.findViewById(R.id.createItemCategorySpinner);
+	    
+	    // get text
+	    String resultText = (String) resultView.getText();
 
+	    // check whether user's selection equals to the result in text view 
+	    assertEquals(resultText,mSelection);
+
+	} 
 	
-	// fill blank
+	
+	// test currency spinner
+	public void testCurrencySpinnerUI() {
+		// run activity
+		activity.runOnUiThread(
+			new Runnable() {
+		        public void run() {
+		        	item_currency.requestFocus();
+		        	item_currency.setSelection(INITIAL_POSITION);
+		        }
+			});
+		
+		// make a selection
+	    this.sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
+	    for (int i = 1; i <= TEST_POSITION; i++) {
+	      this.sendKeys(KeyEvent.KEYCODE_DPAD_DOWN);
+	    } 
+
+	    // send key event
+	    this.sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
+	    
+	    // check the result
+	    mPos = item_currency.getSelectedItemPosition();
+	    mSelection = (String)item_currency.getItemAtPosition(mPos);
+	    
+	    // get the spinner result text view
+	    TextView resultView = (TextView) activity.findViewById(R.id.createCurrencySpinner);
+	    
+	    // get text	    
+	    String resultText = (String) resultView.getText();
+
+	    // check whether user's selection equals to the result in text view 
+	    assertEquals(resultText,mSelection);
+
+	} 
+
+	/*
+	 * End of spinner testing
+	 */
+	
+	/*
+	 * This part is Button testing
+	 * Button id: FinishItemButton
+	 */
+	
+	// test layout of button
+	public void testFinishButton_layout() {
+		// view which is expected to be present on the screen
+		final View decorView = activity.getWindow().getDecorView();
+		
+		// check if it is on screen
+	    ViewAsserts.assertOnScreen(decorView, finish_button);
+
+	    // check whether the Button object's width and height attributes match the expected values
+	    final ViewGroup.LayoutParams layoutParams = finish_button.getLayoutParams();
+	    assertNotNull(layoutParams);
+	    assertEquals(layoutParams.width, WindowManager.LayoutParams.MATCH_PARENT);
+	    assertEquals(layoutParams.height, WindowManager.LayoutParams.WRAP_CONTENT);
+	    assertTrue(View.GONE == finish_button.getVisibility());
+	    
+	} 
+	
+	// test the string shown on button
+	public void testInfoTextView_layout() {
+	    assertTrue(finish_button.getText().equals("Finish"));
+	}
+	
+	// test button behavior
+	public void testFinishButton_expectInfo() {
+		// expected string
+	    String expectedInfoText = activity.getString(R.string.title_activity_claimant_add_item);
+	    
+	    // check if button is visible
+	    TouchUtils.clickView(this, finish_button);
+	    assertTrue(View.VISIBLE == finish_button.getVisibility());
+	    
+	    // check if text shown on button equals expected text
+	    assertEquals(expectedInfoText, finish_button.getText());
+	}
+	
+	// TBC..
+	
+	
+	/*
+	// test button
 	@SuppressWarnings("unused")
 	private void testAddButton(String itemName, String itemDate, String itemCate, String itemDes, String itemAmount, String itemCurr) {
 		assertNotNull(activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.FinishItemButton));
-		int Pos = i_cate.getSelectedItemPosition();
+		int Pos = cate.getSelectedItemPosition();
 		String Selection = (String)i_cate.getItemAtPosition(Pos);
 		TextView resultView =
 			      (TextView) mActivity.findViewById(
@@ -99,5 +269,5 @@ public class ClaimantAddItemUITest extends
 			((Button) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.FinishItemButton)).performClick();
 		    assertNotNull(activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.itemListView));	    
 		}
-
+	*/
 }
