fe2c94f8ae3b9a6f582dd10e4001ee2234db3afc
han95 <han8@ualberta.ca>
2015-03-08 14:05:05 -0600

    add UI test for US02

---
+++ AppTest/src/ca/ualberta/CMPUT301W15T06/test/US02_01_01Test.java
@@ -0,0 +1,123 @@
+package ca.ualberta.CMPUT301W15T06.test;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.app.Instrumentation.ActivityMonitor;
+import android.content.Intent;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.ViewAsserts;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import ca.ualberta.CMPUT301W15T06.ClaimantClaimListActivity;
+import ca.ualberta.CMPUT301W15T06.MainActivity;
+
+public class US02_01_01Test extends ActivityInstrumentationTestCase2<MainActivity>{
+
+	public US02_01_01Test() {
+		super(MainActivity.class);
+	}
+	
+	Button ClaimantButton;
+	Instrumentation instrumentation;
+	Activity activity;
+	EditText textInput;
+	ListView gv;
+
+	//set up
+	protected void setUp() throws Exception {
+		
+		super.setUp();
+		instrumentation = getInstrumentation();
+		activity = getActivity();
+		ClaimantButton = (Button) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.claimantButton);
+		setActivityInitialTouchMode(false);
+
+	}
+	
+	/*
+	 * Test for US02.01.01 Basic Flow 1
+	 */
+	
+	//test Claimant Button layout
+	public void testClaimantButtonlayout() {
+	    final View decorView = activity.getWindow().getDecorView();
+
+	    ViewAsserts.assertOnScreen(decorView, ClaimantButton);
+
+	    final ViewGroup.LayoutParams layoutParams =
+	           ClaimantButton.getLayoutParams();
+	    assertNotNull(layoutParams);
+	    assertEquals(layoutParams.width, WindowManager.LayoutParams.WRAP_CONTENT);
+	    assertEquals(layoutParams.height, WindowManager.LayoutParams.WRAP_CONTENT);
+	    
+	    assertEquals("Incorrect label of the button", "Claimant", ClaimantButton.getText());
+	}
+	
+	/*
+	 * Test Case for US02.01.01 Basic Flow 2
+	 */
+	public void testClickClaimantButton() {
+		
+		// test if the button can create next activity
+		// monitor
+		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(ClaimantClaimListActivity.class.getName(), null, false);
+		
+		// open current activity.
+		activity.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+			  // click button and open next activity.
+			  ClaimantButton.performClick();
+		  }
+		});
+		
+		ClaimantClaimListActivity nextActivity = (ClaimantClaimListActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 10000);
+		// next activity is opened and captured.
+		assertNotNull(nextActivity);
+		nextActivity.finish();
+	}
+	
+	/*
+	 * Test for US02.01.01 Basic Flow 3
+	 */
+	
+	public void testClaimList() {
+		
+		// test if the button can create next activity
+		// register next activity that need to be monitored.
+		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(ClaimantClaimListActivity.class.getName(), null, false);
+
+		// open current activity.
+		activity.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+			  // click button and open next activity.
+			  ClaimantButton.performClick();
+		  }
+		});
+
+		ClaimantClaimListActivity nextActivity = (ClaimantClaimListActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 10000);
+		// next activity is opened and captured.
+		assertNotNull(nextActivity);
+		
+    	// view which is expected to be present on the screen
+		final View decorView = nextActivity.getWindow().getDecorView();	  
+	    ListView claimList = (ListView) nextActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.claimListView);
+	    
+	    // check if claim list is on screen
+	    ViewAsserts.assertOnScreen(decorView, claimList);
+	    
+		// check whether the Button object's width and height attributes match the expected values
+	    final ViewGroup.LayoutParams layoutParams = claimList.getLayoutParams();
+	    
+	    assertNotNull(layoutParams);
+	    assertEquals(layoutParams.width, WindowManager.LayoutParams.MATCH_PARENT);
+	    assertEquals(layoutParams.height, WindowManager.LayoutParams.WRAP_CONTENT);
+	    
+	    nextActivity.finish();
+	}
+}
