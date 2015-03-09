ac481c5689e8e25b4828e47d87712c3e54ebb98a
han95 <han8@ualberta.ca>
2015-03-09 14:45:14 -0600

    modify UI test for US07.01.01, add other UI tests for US07, UNFINISHED!

--- AppTest/src/ca/ualberta/CMPUT301W15T06/test/US07_01_01_Test_NOTFINISHED.java
+++ AppTest/src/ca/ualberta/CMPUT301W15T06/test/US07_01_01_Test_NOTFINISHED.java
@@ -1,5 +1,7 @@
 package ca.ualberta.CMPUT301W15T06.test;
 
+import java.lang.reflect.Proxy;
+
 import android.app.Activity;
 import android.app.Instrumentation;
 import android.app.Instrumentation.ActivityMonitor;
@@ -16,9 +18,9 @@ import android.widget.ListView;
 import ca.ualberta.CMPUT301W15T06.ClaimantClaimListActivity;
 import ca.ualberta.CMPUT301W15T06.MainActivity;
 
-public class US07_01_01Test extends ActivityInstrumentationTestCase2<MainActivity>{
+public class US07_01_01_Test_NOTFINISHED extends ActivityInstrumentationTestCase2<MainActivity>{
 
-	public US07_01_01Test() {
+	public US07_01_01_Test_NOTFINISHED() {
 		super(MainActivity.class);
 	}
 	
@@ -77,8 +79,12 @@ public class US07_01_01Test extends ActivityInstrumentationTestCase2<MainActivit
 	    assertEquals(layoutParams.height, WindowManager.LayoutParams.WRAP_CONTENT);
 	    
 	    nextActivity.finish();
+	    activity.finish();
 	}
 	
+	/*
+	 * Test for US07.01.01 Basic Flow 2
+	 */
 	
 	public void testLongClickClaimList() {
 		
@@ -105,40 +111,51 @@ public class US07_01_01Test extends ActivityInstrumentationTestCase2<MainActivit
 			}
 		});
 		
-	    
-	    nextActivity.finish();
-	}
-	
+		/*
+		 * Test for US07.03.01 Basic Flow 3
+		 * 
+		 * WARNING: missing function
+		 */
 
-//	public void testContextMenu() {
-//		
-//		// test if the button can create next activity
-//		// register next activity that need to be monitored.
-//		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(ClaimantClaimListActivity.class.getName(), null, false);
-//
-//		// open current activity.
-//		activity.runOnUiThread(new Runnable() {
-//			@Override
-//			public void run() {
-//			  // click button and open next activity.
-//			  ClaimantButton.performClick();
-//		  }
-//		});
-//
-//		ClaimantClaimListActivity nextActivity = (ClaimantClaimListActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 10000);
-//		
-//		final ListView claimList = (ListView) nextActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.claimListView);
-//		nextActivity.runOnUiThread(new Runnable() {
-//			@Override
-//			public void run() {
-//				claimList.performLongClick();
-//			}
-//		});
-//		
-//		final Context contextMenu = (Context) getActivity().getBaseContext();
+//		final ContextMenu contextMenu = nextActivity.getContextMenu();
 //		assertTrue(contextMenu != null);
 //		
-//	    
-//	    nextActivity.finish();
-//	}
+//
+
+		/*
+		 * Test for US07.03.01 Basic Flow 4
+		 */
+		
+//	    getActivity().runOnUiThread(new Runnable() {
+//	        public void run() {
+//	            contextMenu.performIdentifierAction(ca.ualberta.CMPUT301W15T06.R.id.add_new_claim, 0);
+//	        }
+//	    });
+//	    getInstrumentation().waitForIdleSync();
+		
+
+		
+		/*
+		 * Test for US07.01.01 Basic Flow 5
+		 */
+		
+    	// view which is expected to be present on the screen
+		final View decorView = nextActivity.getWindow().getDecorView();	  
+	    ListView updatedclaimList = (ListView) nextActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.claimListView);
+	    
+	    // check if claim list is on screen
+	    ViewAsserts.assertOnScreen(decorView, updatedclaimList);
+	    
+		// check whether the Button object's width and height attributes match the expected values
+	    final ViewGroup.LayoutParams layoutParams = updatedclaimList.getLayoutParams();
+	    
+	    assertNotNull(layoutParams);
+	    assertEquals(layoutParams.width, WindowManager.LayoutParams.MATCH_PARENT);
+	    assertEquals(layoutParams.height, WindowManager.LayoutParams.WRAP_CONTENT);
+	    
+	    nextActivity.finish();
+	    activity.finish();
+		
+	}
+	
 }
---
+++ AppTest/src/ca/ualberta/CMPUT301W15T06/test/US07_02_01_Test_NOTFINISHED.java
@@ -0,0 +1,199 @@
+package ca.ualberta.CMPUT301W15T06.test;
+
+import java.lang.reflect.Proxy;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Instrumentation;
+import android.app.Instrumentation.ActivityMonitor;
+import android.content.Context;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.test.ViewAsserts;
+import android.view.ContextMenu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import ca.ualberta.CMPUT301W15T06.ClaimantClaimListActivity;
+import ca.ualberta.CMPUT301W15T06.MainActivity;
+
+public class US07_02_01_Test_NOTFINISHED extends ActivityInstrumentationTestCase2<MainActivity>{
+
+	public US07_02_01_Test_NOTFINISHED() {
+		super(MainActivity.class);
+	}
+	
+	Button ClaimantButton;
+	Instrumentation instrumentation;
+	Activity activity;
+	EditText textInput;
+	ListView gv;
+
+	//set up
+	protected void setUp() throws Exception {
+		
+		super.setUp();
+		instrumentation = getInstrumentation();
+		activity = getActivity();
+		ClaimantButton = (Button) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.claimantButton);
+		setActivityInitialTouchMode(false);
+
+	}
+	
+	/*
+	 * Test for US07.02.01 Basic Flow 1
+	 */
+	
+	public void testClaimList() {
+		
+		// test if the button can create next activity
+		// register next activity that need to be monitored.
+		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(ClaimantClaimListActivity.class.getName(), null, false);
+
+		// open current activity.
+		activity.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+			  // click button and open next activity.
+			  ClaimantButton.performClick();
+		  }
+		});
+
+		ClaimantClaimListActivity nextActivity = (ClaimantClaimListActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 10000);
+		// next activity is opened and captured.
+		assertNotNull(nextActivity);
+		
+    	// view which is expected to be present on the screen
+		final View decorView = nextActivity.getWindow().getDecorView();	  
+	    ListView claimList = (ListView) nextActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.claimListView);
+	    
+	    // check if claim list is on screen
+	    ViewAsserts.assertOnScreen(decorView, claimList);
+	    
+		// check whether the Button object's width and height attributes match the expected values
+	    final ViewGroup.LayoutParams layoutParams = claimList.getLayoutParams();
+	    
+	    assertNotNull(layoutParams);
+	    assertEquals(layoutParams.width, WindowManager.LayoutParams.MATCH_PARENT);
+	    assertEquals(layoutParams.height, WindowManager.LayoutParams.WRAP_CONTENT);
+	    
+	    nextActivity.finish();
+	    activity.finish();
+	}
+	
+	/*
+	 * Test for US07.02.01 Basic Flow 2
+	 */
+	
+	public void testLongClickClaimList() {
+		
+		// test if the button can create next activity
+		// register next activity that need to be monitored.
+		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(ClaimantClaimListActivity.class.getName(), null, false);
+
+		// open current activity.
+		activity.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+			  // click button and open next activity.
+			  ClaimantButton.performClick();
+		  }
+		});
+
+		ClaimantClaimListActivity nextActivity = (ClaimantClaimListActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 10000);
+		
+		final ListView claimList = (ListView) nextActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.claimListView);
+		nextActivity.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				assertNotNull(claimList.performLongClick());
+			}
+		});
+		
+		/*
+		 * Test for US07.03.01 Basic Flow 3
+		 * 
+		 * WARNING: missing function
+		 */
+
+//		final ContextMenu contextMenu = nextActivity.getContextMenu();
+//		assertTrue(contextMenu != null);
+//		
+//
+//	    getActivity().runOnUiThread(new Runnable() {
+//	        public void run() {
+//	            contextMenu.performIdentifierAction(ca.ualberta.CMPUT301W15T06.R.id.add_new_claim, 0);
+//	        }
+//	    });
+//	    getInstrumentation().waitForIdleSync();
+
+		/*
+		 * Test for US07.03.01 Basic Flow 4
+		 */
+		
+//	    getActivity().runOnUiThread(new Runnable() {
+//	        public void run() {
+//	            contextMenu.performIdentifierAction(ca.ualberta.CMPUT301W15T06.R.id.add_new_claim, 0);
+//	        }
+//	    });
+//	    getInstrumentation().waitForIdleSync();
+		
+
+		
+		/*
+		 * Test for US07.02.01 Basic Flow 5
+		 */
+		
+//		View mLLAppelerFixe = nextActivity.
+//		
+//		nextActivity.runOnUiThread(new Runnable() {
+//	        @Override
+//	        public void run() {
+//	            View mLLAppelerFixe;
+//				assertTrue(mLLAppelerFixe.performClick());
+//
+//	            AlertDialog mDialog = nextActivity.getAdAppelerFixe();
+//	            assertTrue(mDialog.isShowing());
+//
+//	            Button okButton = mDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+//
+//	            assertTrue(okButton.performClick());
+//	            assertTrue(nextActivity.isNumeroValide());
+//	        }
+//	    });
+		
+		/*
+		 * Test for US07.02.01 Basic Flow 6
+		 */
+		
+		//????????????????????????????????????????????/
+		
+		/*
+		 * Test for US07.02.01 Basic Flow 7
+		 */
+		
+    	// view which is expected to be present on the screen
+		final View decorView = nextActivity.getWindow().getDecorView();	  
+	    ListView updatedclaimList = (ListView) nextActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.claimListView);
+	    
+	    // check if claim list is on screen
+	    ViewAsserts.assertOnScreen(decorView, updatedclaimList);
+	    
+		// check whether the Button object's width and height attributes match the expected values
+	    final ViewGroup.LayoutParams layoutParams = updatedclaimList.getLayoutParams();
+	    
+	    assertNotNull(layoutParams);
+	    assertEquals(layoutParams.width, WindowManager.LayoutParams.MATCH_PARENT);
+	    assertEquals(layoutParams.height, WindowManager.LayoutParams.WRAP_CONTENT);
+	    
+	    nextActivity.finish();
+	    activity.finish();
+
+	}
+	
+
+
+}
