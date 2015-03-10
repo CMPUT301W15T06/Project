c1422958a12a259f18315d06f6478776887a6198
agileshaw <agileshaw@users.noreply.github.com>
2015-03-10 12:19:43 -0600

    Update Javadoc for Claim.java

--- App/src/ca/ualberta/CMPUT301W15T06/Claim.java
+++ App/src/ca/ualberta/CMPUT301W15T06/Claim.java
@@ -51,7 +51,7 @@ public class Claim extends AppModel{
 	 * and endDate to record the begining and ending date for travel. Set private
 	 * Sting status to track whether the Claim travel status and the default value 
 	 * is "In progress".
-	*/
+	 */
 	private String name;
 	private String beginDate;
 	private String endDate;
@@ -62,20 +62,20 @@ public class Claim extends AppModel{
 	 * information, including location and reason. 
 	 * 
 	 * @see java.util.ArrayList
-	*/
+	 */
 	private ArrayList<Item> itemList;
 	private ArrayList<Destination> destinationList;
 	/**
 	 * Set private String approver to track if the claim is approved. Set private
 	 * String comment to add comment to the Claim.
-	*/
+	 */
 	private String approver;
 	private String comment;	
 	/**
 	 * Set private ArrayList named tagList which contains all the tags of the Claim.
 	 * 
 	 * @see java.util.ArrayList
-	*/
+	 */
 	private ArrayList<Tag> tagList;
 	
 
@@ -85,7 +85,7 @@ public class Claim extends AppModel{
 	 * 
 	 * @param claimName  a String variable
 	 * @see java.util.ArrayList
-	*/
+	 */
 	public Claim(String claimName) {
 		super();
 		itemList=new ArrayList<Item>();
@@ -101,7 +101,7 @@ public class Claim extends AppModel{
 	 * Claim or editing a current Claim.
 	 * 
 	 * @param name  a String variable
-	*/
+	 */
 	public void setName(String name){
 		this.name=name;
 		notifyListeners();
@@ -109,10 +109,10 @@ public class Claim extends AppModel{
 	
 	/**
 	 * Return the string variable name. This method will be used when 
-	 * other class need to use the name. 
+	 * other class need to use or display the name. 
 	 * 
 	 * @return name  a String variable
-	*/
+	 */
 	public String getName() {
 		return name;
 	}
@@ -124,7 +124,7 @@ public class Claim extends AppModel{
 	 * date to a new Claim or editing a current Claim.
 	 * 
 	 * @param beginDate  a String variable
-	*/
+	 */
 	public void setBeginDate(String beginDate){
 		this.beginDate=beginDate;
 		notifyListeners();
@@ -132,10 +132,10 @@ public class Claim extends AppModel{
 	
 	/**
 	 * Return the string variable beginDate. This method will be used when 
-	 * other class need to use the beginDate. 
+	 * other class need to use or display the beginDate. 
 	 * 
 	 * @return beginDate  a String variable
-	*/
+	 */
 	public String getBeginDate() {
 		return beginDate;
 	}
@@ -147,7 +147,7 @@ public class Claim extends AppModel{
 	 * date to a new Claim or editing a current Claim.
 	 * 
 	 * @param endDate  a String variable
-	*/
+	 */
 	public void setEndDate(String endDate){
 		this.endDate=endDate;
 		notifyListeners();
@@ -155,10 +155,10 @@ public class Claim extends AppModel{
 	
 	/**
 	 * Return the string variable endDate. This method will be used when 
-	 * other class need to use the endDate. 
+	 * other class need to use or display the endDate. 
 	 * 
 	 * @return endDate  a String variable
-	*/
+	 */
 	public String getEndDate() {
 		return endDate;
 	}
@@ -170,7 +170,7 @@ public class Claim extends AppModel{
 	 * value of status is "In progress".
 	 * 
 	 * @param status  a String variable
-	*/
+	 */
 	public void setStatus(String status){
 		this.status=status;
 		notifyListeners();
@@ -178,25 +178,44 @@ public class Claim extends AppModel{
 	
 	/**
 	 * Return the string variable status. This method will be used when 
-	 * other class need to use the status. 
+	 * other class need to use or display the status. 
 	 * 
 	 * @return status  a String variable
-	*/
+	 */
 	public String getStatus(){
 		return status;	
 	}
 
-	
-	
+	/**
+	 * Return the ArrayList itemList. This method will be used when
+	 * other class need to use or display the itemList. 
+	 * 
+	 * @see java.util.ArrayList
+	 * @return itemList  an ArrayList object.
+	 */
 	public ArrayList<Item> getItemList() {
 		return itemList;
 	}
 	
-
+	/**
+	 * Return the ArrayList destinationList. This method will be used when
+	 * other class need to use or display the destinationList. 
+	 * 
+	 * @see java.util.ArrayList
+	 * @return destinationList  an ArrayList object.
+	 */
 	public ArrayList<Destination> getDestinationList() {
 		return destinationList;
 	}
 	
+	/**
+	 * This method uses <code>getName()</code> to combine all Destination in destinationList
+	 * and Tag in tagList to a big String. Use <code>getCM()</code> to dispaly the amount 
+	 * with coresponding currency. It will be operate when the program need the 
+	 * destinationList and tagList to display.
+	 * 
+	 * @return ""  a string object combine with many small string varrable
+	 */
 	public String toString(){
 		String dest="";
 		for (Destination d:destinationList){
@@ -211,6 +230,15 @@ public class Claim extends AppModel{
 
 	}
 	
+	/**
+	 * Calculate the total amount and return the value. This method first 
+	 * set up a Double variable total with a default value null, then 
+	 * use <code>getCurrency()</code> and <code>getAmount()</code> method 
+	 * to get a total amount of the Calim and return it using <code>toString()</code>. 
+	 * 
+	 * @param currency  a String variable
+	 * @return total  a Double variable
+	 */
 	public String getCM(String currency){
 		Double total = null;
 
@@ -227,69 +255,169 @@ public class Claim extends AppModel{
 		return total==null?(currency+": "):(currency+": "+total.toString());
 	}
 	
+	/**
+	 * Return the ArrayList cLaimDetail as null. This method will be used when 
+	 * other class need to use or dispaly the detail of the Claim. 
+	 * 
+	 * @see java.util.ArrayList
+	 * @return null  an ArrayList object
+	 */
 	public ArrayList<Item> getClaimDetail() {
 		return null;
 		
 	}
 	
+	/**
+	 * Set the approver using a String variable name.
+	 * 
+	 * @param name  a String variable
+	 */
 	public void setApprover(String name){
 		
 	}
 	
+	/**
+	 * Return the approver as null. This method will be used when
+	 * other class need to use the approver. 
+	 * 
+	 * @return null  a String variable
+	 */
 	public String getApprover(){
 		return null;
 	}
 	
+	/**
+	 * Set comment to a String comment.
+	 * 
+	 * @param comment  a String variable
+	 */
 	public void setComment(String comment){
 		
 	}
 	
+	/**
+	 * Return the String comment as null. This method will be used when
+	 * other class need to use or display the comment. 
+	 * 
+	 * @return null  a String variable
+	 */
 	public String getComment(){
 		return null;
 	}
 	
+	/**
+	 * Add a String tagName to tagList as new Tag. 
+	 * 
+	 * @param tagName  a String variable
+	 */
 	public void addTag(String tagName){
 		
 	}
 	
+	/**
+	 * Return the ArrayList tagList as null. This method will be used when 
+	 * other class need to use or dispaly the list of Tag. 
+	 * 
+	 * @see java.util.ArrayList
+	 * @return null  an ArrayList object
+	 */
 	public ArrayList<Tag> getTagList(){		
 		return null;
 	}	
 
+	/**
+	 * Return a boolean variable as false. This method will be used when 
+	 * other class need to justify whether the Claim can be edited or not. 
+	 * 
+	 * @return false  a boolean variable
+	 */
 	public boolean getEdiable(){
 		return false;
 	}
 
+	/**
+	 * Return the String string as null. This method will be used when
+	 * other class need to use or display the item. 
+	 * 
+	 * @param string  a String variable
+	 * @return null  a String variable
+	 */
 	public Object getItem(String string) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Return the Item object itemDate as null. This method will 
+	 * be used when other class need to use or display the date of 
+	 * the item expense. 
+	 * 
+	 * @param expenseTest1  an Item object
+	 * @return null  an Item object
+	 */
 	public Object getItemDate(Item expenseTest1) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
+	/**
+	 * Return the String itemCategory as null. This method will be 
+	 * used when other class need to use or display the category of 
+	 * the item expense. 
+	 * 
+	 * @param expenseTest1  an Item object
+	 * @return null  a String Variable
+	 */
 	public String getItemCategory(Item expenseTest1) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Return the Item object itemAmount as null. This method will 
+	 * be used when other class need to use or display the amount of 
+	 * the item expense. 
+	 * 
+	 * @param expenseTest1  an Item object
+	 * @return null  an Item object
+	 */
 	public Object getItemAmount(Item expenseTest1) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Return the String itemCurrency as null. This method will 
+	 * be used when other class need to use or display the currency of 
+	 * the item expense. 
+	 * 
+	 * @param expenseTest1  an Item object
+	 * @return null  a String variable
+	 */
 	public String getItemCurrency(Item expenseTest1) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Return the String itemDescription as null. This method will 
+	 * be used when other class need to use or display the description of 
+	 * the item expense. 
+	 * 
+	 * @param expenseTest1  an Item object
+	 * @return null  a String variable
+	 */
 	public String getItemDescription(Item expenseTest1) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Return the int itemSize. This method will be used when 
+	 * other class need to use or display the size of the item. 
+	 * 
+	 * @return <code>itemList.size()</code>  an int variable
+	 */
 	public int getItemSize() {
 		// TODO Auto-generated method stub
 		return itemList.size();
