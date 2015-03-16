bc5719af007fc4772042e21da2cde57fc4a3f972
annypanny <anny_pan@live.cn>
2015-03-15 21:00:08 -0600

    us04.06.01 ui test

--- AppTest/src/ca/ualberta/CMPUT301W15T06/test/US04_06_01_Test.java
+++ AppTest/src/ca/ualberta/CMPUT301W15T06/test/US04_06_01_Test.java
@@ -5,6 +5,7 @@ import ca.ualberta.CMPUT301W15T06.ClaimList;
 import ca.ualberta.CMPUT301W15T06.ClaimantAddClaimController;
 import ca.ualberta.CMPUT301W15T06.ClaimantAddItemController;
 import ca.ualberta.CMPUT301W15T06.ClaimantClaimListActivity;
+import ca.ualberta.CMPUT301W15T06.ClaimantEditItemActivity;
 import ca.ualberta.CMPUT301W15T06.ClaimantItemDetailActivity;
 import ca.ualberta.CMPUT301W15T06.ClaimantItemListActivity;
 import ca.ualberta.CMPUT301W15T06.MainActivity;
@@ -19,11 +20,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class US04_05_01_Test extends
+public class US04_06_01_Test extends
 		ActivityInstrumentationTestCase2<MainActivity> {
 	
 	Instrumentation instrumentation;
@@ -33,7 +36,7 @@ public class US04_05_01_Test extends
 	Intent intent;
 	ListView clv;
 	
-	public US04_05_01_Test () {
+	public US04_06_01_Test () {
 		super(MainActivity.class);
 	}
 	
@@ -50,14 +53,14 @@ public class US04_05_01_Test extends
 	}
 
 	/*
-	 * Test for US04.05.01 Basic Flow 1
+	 * Test for US04.06.01 Basic Flow 1
 	 */
 
 	// test button exists
 	public void testLayout() {
 		assertNotNull(activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.claimantButton));
 		assertNotNull(activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.approverButton));
-		
+				
 		// test Approver button layout
 		final View decorView = activity.getWindow().getDecorView();
 		ViewAsserts.assertOnScreen(decorView, ApproverButton);
@@ -68,14 +71,14 @@ public class US04_05_01_Test extends
 
 		Button view = (Button) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.approverButton);
 		assertEquals("Incorrect label of the button", "Approver",view.getText());
-				
+					
 		/*
-		 * Test for US04.05.01 Basic Flow 2
+		 * Test for US04.06.01 Basic Flow 2
 		 */
-			
+				
 		// test Claimant Button layout
 		ViewAsserts.assertOnScreen(decorView, ClaimantButton);
-		
+
 		final ViewGroup.LayoutParams layoutParams1 = ClaimantButton.getLayoutParams();
 		assertNotNull(layoutParams1);
 		assertEquals(layoutParams1.width,WindowManager.LayoutParams.WRAP_CONTENT);
@@ -83,7 +86,7 @@ public class US04_05_01_Test extends
 
 		Button view1 = (Button) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.claimantButton);
 		assertEquals("Incorrect label of the button", "Claimant",view1.getText());
-				
+					
 		// test if the button can create next activity
 		// register next activity that need to be monitored.
 		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(ClaimantClaimListActivity.class.getName(), null, false);
@@ -100,22 +103,22 @@ public class US04_05_01_Test extends
 		});
 
 		ClaimantClaimListActivity nextActivity = (ClaimantClaimListActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 10000);
-			
+				
 		// next activity is opened and captured.
 		assertNotNull(nextActivity);
 
 		/*
-		 * Test for US04.05.01 Basic Flow 3
+		 * Test for US04.06.01 Basic Flow 3
 		 */
 
 		// view which is expected to be present on the screen
 		final View decorView2 = nextActivity.getWindow().getDecorView();
 
 		clv = (ListView) nextActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.claimListView);
-			
+				
 		// check if it is on screen
 		ViewAsserts.assertOnScreen(decorView2, clv);
-		
+				
 		// check whether the Button object's width and height attributes match
 		// the expected values
 		final ViewGroup.LayoutParams layoutParams2 = clv.getLayoutParams();
@@ -123,47 +126,47 @@ public class US04_05_01_Test extends
 		assertEquals(layoutParams2.height,WindowManager.LayoutParams.WRAP_CONTENT);
 
 		/*
-		 * Test for US04.05.01 Basic Flow 4
+		 * Test for US04.06.01 Basic Flow 4
 		 */
 		final ListView claimList = (ListView) nextActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.claimListView);
 		assertNotNull(claimList.performClick());
 		nextActivity.runOnUiThread(new Runnable() {
 			@Override
 			public void run() {
-						
+							
 				// build controller
 				ClaimantAddClaimController cacc = new ClaimantAddClaimController(new ClaimList());
 					
 				// get ClaimantClaimListActivity
 				ActivityMonitor activityMonitor = getInstrumentation().addMonitor(ClaimantClaimListActivity.class.getName(),null, false);
 				final ClaimantClaimListActivity clActivity = (ClaimantClaimListActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 10000);
-					
+						
 				// get claimListView
 				final ListView claimList = (ListView) clActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.claimListView);
 
 				// add new claim, position: index 0
 				cacc.addClaim("test", "2014-12-11", "2015-01-05");
-					
+						
 				// click new claim
 				assertNotNull(claimList.getChildAt(0).performClick());
-								
+									
 				/*
-				 * Test for US04.05.01 Basic Flow 5
+				 * Test for US04.06.01 Basic Flow 5
 				 */
-						
+							
 				// get ClaimantItemListActivity
 				ActivityMonitor activityMonitor1 = getInstrumentation().addMonitor(ClaimantItemListActivity.class.getName(),null, false);
 				final ClaimantItemListActivity ilActivity = (ClaimantItemListActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor1, 10000);
-						
+							
 				assertNotNull(ilActivity);
-					
+							
 				// get item list view
 				// view which is expected to be present on the screen
 				final View decorView3 = ilActivity.getWindow().getDecorView();
 				final ListView itemList = (ListView) ilActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.itemListView);	
 				// check if it is on screen
 				ViewAsserts.assertOnScreen(decorView3, itemList);
-					
+						
 				// check whether the Button object's width and height attributes match
 				// the expected values
 				final ViewGroup.LayoutParams layoutParams2 = clv.getLayoutParams();
@@ -171,23 +174,23 @@ public class US04_05_01_Test extends
 				assertEquals(layoutParams2.height,WindowManager.LayoutParams.WRAP_CONTENT);
 
 				/*
-				 * Test for US04.05.01 Basic Flow 6
+				 * Test for US04.06.01 Basic Flow 6
 				 */
-					
+						
 				final ListView itemList1 = (ListView) ilActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.itemListView);
 				assertNotNull(itemList1.performClick());
 				ilActivity.runOnUiThread(new Runnable() {
-				@Override
+					@Override
 					public void run() {
-								
+									
 						// build controller
 						Claim claim = new Claim("test");
 						ClaimantAddItemController caic = new ClaimantAddItemController(claim);
-							
+								
 						// get ClaimantItemListActivity
 						ActivityMonitor activityMonitor1 = getInstrumentation().addMonitor(ClaimantItemListActivity.class.getName(),null, false);
 						final ClaimantItemListActivity ilActivity1 = (ClaimantItemListActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor1, 10000);
-							
+								
 						// get itemListView
 						final ListView itemList1 = (ListView) ilActivity1.findViewById(ca.ualberta.CMPUT301W15T06.R.id.itemListView);
 
@@ -198,24 +201,24 @@ public class US04_05_01_Test extends
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
-							
+								
 						// click new item
 						assertNotNull(itemList1.getChildAt(0).performClick());
-							
+								
 						/*
-						 * Test for US04.05.01 Basic Flow 7
+						 * Test for US04.06.01 Basic Flow 7
 						 */
-							
+								
 						// get contextMenu
 						final GridView contextMenu = (GridView) ilActivity1.findViewById(ca.ualberta.CMPUT301W15T06.R.array.item_dialog_array);
 
 						// check if user can click this context menu
 						assertNotNull(contextMenu.performClick());
-							
+								
 						/*
-						 * Test for US04.05.01 Basic Flow 8
+						 * Test for US04.06.01 Basic Flow 8
 						 */
-							
+								
 						ilActivity1.runOnUiThread(new Runnable(){
 
 							@Override
@@ -223,47 +226,96 @@ public class US04_05_01_Test extends
 								contextMenu.performItemClick(contextMenu, 2, claimList.getAdapter().getItemId(0));
 							}
 						});
-							
+								
 						/*
-						 * Test for US04.05.01 Basic Flow 9
+						 * Test for US04.06.01 Basic Flow 9
 						 */
-							
+								
 						// get ClaimantItemListActivity
 						ActivityMonitor activityMonitor11 = getInstrumentation().addMonitor(ClaimantItemDetailActivity.class.getName(),null, false);
 						final ClaimantItemDetailActivity iDetailActivity = (ClaimantItemDetailActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor11, 10000);
-							
+								
 						assertNotNull(iDetailActivity);
-							
+								
 
 						// view which is expected to be present on the screen
 						final View decorView11 = iDetailActivity.getWindow().getDecorView();
-							
+								
 						final TextView iDDate = (TextView) iDetailActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.itemDateVTextView);	
 						// check if it is on screen
 						ViewAsserts.assertOnScreen(decorView11, iDDate);
-							
+								
 						final TextView iDCat = (TextView) iDetailActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.itemCategoryVTextView);	
 						// check if it is on screen
 						ViewAsserts.assertOnScreen(decorView11, iDCat);
-							
+								
 						final TextView iDDes = (TextView) iDetailActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.itemDescriptionVTextView);	
 						// check if it is on screen
 						ViewAsserts.assertOnScreen(decorView11, iDDes);
-							
+								
 						final TextView iDAmount = (TextView) iDetailActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.itemAmountVTextView);	
 						// check if it is on screen
 						ViewAsserts.assertOnScreen(decorView11, iDAmount);
-							
+								
 						final TextView iDCurr = (TextView) iDetailActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.itemCurrencyVTextView);	
 						// check if it is on screen
 						ViewAsserts.assertOnScreen(decorView11, iDCurr);
-					
-					
-					
-			}
-		});
-			}
-		});
-	}
+						
+						/*
+						 * Test for US04.06.01 Basic Flow 10
+						 */
+						
+						
+						final Button editItem = (Button) iDetailActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.editItemButton);
+						ilActivity.runOnUiThread(new Runnable() {
+							@Override
+							public void run() {
+								// click button and open next activity.
+								editItem.performClick();
+							}
+						});
+						
+						/*
+						 * Test for US04.06.01 Basic Flow 11
+						 */
+						
+						ActivityMonitor activityMonitor111 = getInstrumentation().addMonitor(ClaimantEditItemActivity.class.getName(),null, false);
+						ClaimantEditItemActivity iEditActivity = (ClaimantEditItemActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor111, 10000);
+						// next activity is opened and captured.
+						assertNotNull(iEditActivity);
+
+						// view which is expected to be present on the screen
+						final View decorView111 = iEditActivity.getWindow().getDecorView();
+						
+						EditText iDate = (EditText) iEditActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.editItemDateEditText);
+						Spinner iCat = (Spinner) iEditActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.editCategorySpinner);
+						EditText iDes = (EditText) iEditActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.editItemDescriptionEditText);
+						EditText iAmount = (EditText) iEditActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.editItemAmountEditText);
+						Spinner iCurr = (Spinner) iEditActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.editCurrencySpinner);
+						
+						// check if it is on screen
+						ViewAsserts.assertOnScreen(decorView111, iDate);
+						assertNotNull(iDate.getVisibility());
+						
+						ViewAsserts.assertOnScreen(decorView111, iCat);
+						assertNotNull(iCat.getVisibility());
+						
+						ViewAsserts.assertOnScreen(decorView111, iDes);
+						assertNotNull(iDes.getVisibility());
+						
+						ViewAsserts.assertOnScreen(decorView111, iAmount);
+						assertNotNull(iAmount.getVisibility());
+						
+						ViewAsserts.assertOnScreen(decorView111, iCurr);
+						assertNotNull(iCurr.getVisibility());
+						
+						
+						
+				}
+			});
+				}
+			});
+		}
+
 
 }
