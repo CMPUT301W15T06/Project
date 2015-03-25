100d1da61c5b014755b1424eb233e7b28544de6f
anny-panny <anny_pan@live.cn>
2015-03-25 13:19:36 -0600

    add us06.01.01 test

--- AppTest/src/ca/ualberta/CMPUT301W15T06/test/TagInClaimTest.java
+++ AppTest/src/ca/ualberta/CMPUT301W15T06/test/TagInClaimTest.java
@@ -1,85 +1,85 @@
-/*
-UA CMPUT 301 Project Group: CMPUT301W15T06
-
-Copyright {2015} {Jingjiao Ni
-
-              Tianqi Xiao
-
-              Jiafeng Wu
-
-              Xinyi Pan 
-
-              Xinyi Wu
-
-              Han Wang}
-Licensed under the Apache License, Version 2.0 (the "License");
-
-you may not use this file except in compliance with the License.
-
-You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
-Unless required by applicable law or agreed to in writing, software distributed under 
-the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF 
-ANY KIND, either express or implied. See the License for the specific language 
-governing permissions and limitations under the License.
-
- */
-package ca.ualberta.CMPUT301W15T06.test;
-
-import java.util.ArrayList;
-import java.util.Collection;
-
-import junit.framework.TestCase;
-import ca.ualberta.CMPUT301W15T06.Claim;
-import ca.ualberta.CMPUT301W15T06.ClaimList;
-import ca.ualberta.CMPUT301W15T06.ClaimantAddClaimController;
-import ca.ualberta.CMPUT301W15T06.String;
-import ca.ualberta.CMPUT301W15T06.TagList;
-
-
-public class TagInClaimTest extends TestCase {
-	
-	//US03.01.01  check if add tag to a claim
-	public void testAddTagToClaim(){
-		//add tag to tagList
-		TagList tagList = new TagList();
-		String name = "A tag";
-		String testTag = new String(name);
-		tagList.addTags(testTag);
-		Collection<String> tags = tagList.getTags();
-		assertTrue("Tag list added incorrect", name.equals(tags));
-		//add tag to a claim
-		String cname = "Travel";
-		Claim claim = new Claim(cname);
-		claim.addTag(name);
-		ArrayList<String> claimTagList = claim.getTagList();
-		assertTrue("should add a tag to a claim",name.equals(claimTagList));
-	}
-	
-	
-	//US03.03.01
-	public void testSearchByTag(){
-		TagList tagList = new TagList();
-		String name = "A tag";
-		String testTag = new String(name);
-		tagList.addTags(testTag);
-		String cname = "Travel";
-		Claim claim = new Claim(cname);
-		claim.addTag(name);
-		ClaimList claimList = new ClaimList();
-		Claim searchResult = claimList.searchByTag(name);
-		assertTrue("Search by tags does not work", cname.equals(searchResult));		
-	}
-
-	//US09.01.01 check if push online successfully
-	public void testPushOnline(){
-		String name = "Travel";
-		Claim claim = new Claim(name);
-		ClaimList claimList = new ClaimList();
-		ClaimantAddClaimController ClaimList = new ClaimantAddClaimController(claimList);
-		/*Manager initial*/
-		/*laimList.addClaim(name,null,null);*/
-		claimList.pushOnline();
-		assertTrue("Push Online unsuccessfully",claim.equals(claimList.pullOnline()));
-	}
-
-}
+///*
+//UA CMPUT 301 Project Group: CMPUT301W15T06
+//
+//Copyright {2015} {Jingjiao Ni
+//
+//              Tianqi Xiao
+//
+//              Jiafeng Wu
+//
+//              Xinyi Pan 
+//
+//              Xinyi Wu
+//
+//              Han Wang}
+//Licensed under the Apache License, Version 2.0 (the "License");
+//
+//you may not use this file except in compliance with the License.
+//
+//You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
+//Unless required by applicable law or agreed to in writing, software distributed under 
+//the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF 
+//ANY KIND, either express or implied. See the License for the specific language 
+//governing permissions and limitations under the License.
+//
+// */
+//package ca.ualberta.CMPUT301W15T06.test;
+//
+//import java.util.ArrayList;
+//import java.util.Collection;
+//
+//import junit.framework.TestCase;
+//import ca.ualberta.CMPUT301W15T06.Claim;
+//import ca.ualberta.CMPUT301W15T06.ClaimList;
+//import ca.ualberta.CMPUT301W15T06.ClaimantAddClaimController;
+//import ca.ualberta.CMPUT301W15T06.String;
+//import ca.ualberta.CMPUT301W15T06.TagList;
+//
+//
+//public class TagInClaimTest extends TestCase {
+//	
+//	//US03.01.01  check if add tag to a claim
+//	public void testAddTagToClaim(){
+//		//add tag to tagList
+//		TagList tagList = new TagList();
+//		String name = "A tag";
+//		String testTag = new String(name);
+//		tagList.addTags(testTag);
+//		Collection<String> tags = tagList.getTags();
+//		assertTrue("Tag list added incorrect", name.equals(tags));
+//		//add tag to a claim
+//		String cname = "Travel";
+//		Claim claim = new Claim(cname);
+//		claim.addTag(name);
+//		ArrayList<String> claimTagList = claim.getTagList();
+//		assertTrue("should add a tag to a claim",name.equals(claimTagList));
+//	}
+//	
+//	
+//	//US03.03.01
+//	public void testSearchByTag(){
+//		TagList tagList = new TagList();
+//		String name = "A tag";
+//		String testTag = new String(name);
+//		tagList.addTags(testTag);
+//		String cname = "Travel";
+//		Claim claim = new Claim(cname);
+//		claim.addTag(name);
+//		ClaimList claimList = new ClaimList();
+//		Claim searchResult = claimList.searchByTag(name);
+//		assertTrue("Search by tags does not work", cname.equals(searchResult));		
+//	}
+//
+//	//US09.01.01 check if push online successfully
+//	public void testPushOnline(){
+//		String name = "Travel";
+//		Claim claim = new Claim(name);
+//		ClaimList claimList = new ClaimList();
+//		ClaimantAddClaimController ClaimList = new ClaimantAddClaimController(claimList);
+//		/*Manager initial*/
+//		/*laimList.addClaim(name,null,null);*/
+//		claimList.pushOnline();
+//		assertTrue("Push Online unsuccessfully",claim.equals(claimList.pullOnline()));
+//	}
+//
+//}
--- AppTest/src/ca/ualberta/CMPUT301W15T06/test/US06_01_01_Test.java
+++ AppTest/src/ca/ualberta/CMPUT301W15T06/test/US06_01_01_Test.java
@@ -1,13 +1,14 @@
 package ca.ualberta.CMPUT301W15T06.test;
 
-import ca.ualberta.CMPUT301W15T06.ClaimList;
 import ca.ualberta.CMPUT301W15T06.Claim;
+import ca.ualberta.CMPUT301W15T06.ClaimList;
 import ca.ualberta.CMPUT301W15T06.ClaimantAddClaimController;
 import ca.ualberta.CMPUT301W15T06.ClaimantAddItemController;
 import ca.ualberta.CMPUT301W15T06.ClaimantClaimListActivity;
 import ca.ualberta.CMPUT301W15T06.ClaimantItemListActivity;
 import ca.ualberta.CMPUT301W15T06.MainActivity;
 import ca.ualberta.CMPUT301W15T06.StatusException;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Instrumentation;
 import android.app.Instrumentation.ActivityMonitor;
@@ -21,8 +22,10 @@ import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ListView;
 
-public class US04_04_01_Test extends
-		ActivityInstrumentationTestCase2<MainActivity> {
+
+@SuppressLint("CutPasteId")
+public class US06_01_01_Test extends ActivityInstrumentationTestCase2<MainActivity>
+{
 	
 	Instrumentation instrumentation;
 	Activity activity;
@@ -31,10 +34,9 @@ public class US04_04_01_Test extends
 	Intent intent;
 	ListView clv;
 	
-	public US04_04_01_Test () {
+	public US06_01_01_Test() {
 		super(MainActivity.class);
 	}
-	
 	
 	// set up
 	protected void setUp() throws Exception {
@@ -49,14 +51,14 @@ public class US04_04_01_Test extends
 	}
 
 	/*
-	 * Test for US04.04.01 Basic Flow 1
+	 * Test for US06.01.01 Basic Flow 1
 	 */
 
 	// test button exists
 	public void testLayout() {
 		assertNotNull(activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.claimantButton));
 		assertNotNull(activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.approverButton));
-		
+			
 		// test Approver button layout
 		final View decorView = activity.getWindow().getDecorView();
 		ViewAsserts.assertOnScreen(decorView, ApproverButton);
@@ -67,11 +69,11 @@ public class US04_04_01_Test extends
 
 		Button view = (Button) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.approverButton);
 		assertEquals("Incorrect label of the button", "Approver",view.getText());
-			
+				
 		/*
-		 * Test for US04.04.01 Basic Flow 2
+		 * Test for US06.01.01 Basic Flow 2
 		 */
-		
+			
 		// test Claimant Button layout
 		ViewAsserts.assertOnScreen(decorView, ClaimantButton);
 
@@ -82,7 +84,7 @@ public class US04_04_01_Test extends
 
 		Button view1 = (Button) activity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.claimantButton);
 		assertEquals("Incorrect label of the button", "Claimant",view1.getText());
-			
+				
 		// test if the button can create next activity
 		// register next activity that need to be monitored.
 		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(ClaimantClaimListActivity.class.getName(), null, false);
@@ -104,17 +106,17 @@ public class US04_04_01_Test extends
 		assertNotNull(nextActivity);
 
 		/*
-		 * Test for US04.04.01 Basic Flow 3
+		 * Test for US06.01.01 Basic Flow 3
 		 */
 
 		// view which is expected to be present on the screen
 		final View decorView2 = nextActivity.getWindow().getDecorView();
-
-		clv = (ListView) nextActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.claimListView);
 		
+		clv = (ListView) nextActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.claimListView);
+			
 		// check if it is on screen
 		ViewAsserts.assertOnScreen(decorView2, clv);
-		
+			
 		// check whether the Button object's width and height attributes match
 		// the expected values
 		final ViewGroup.LayoutParams layoutParams2 = clv.getLayoutParams();
@@ -122,47 +124,47 @@ public class US04_04_01_Test extends
 		assertEquals(layoutParams2.height,WindowManager.LayoutParams.WRAP_CONTENT);
 
 		/*
-		 * Test for US04.04.01 Basic Flow 4
+		 * Test for US06.01.01 Basic Flow 4
 		 */
 		final ListView claimList = (ListView) nextActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.claimListView);
 		assertNotNull(claimList.performClick());
 		nextActivity.runOnUiThread(new Runnable() {
 			@Override
 			public void run() {
-					
+						
 				// build controller
 				ClaimantAddClaimController cacc = new ClaimantAddClaimController(new ClaimList());
-				
+					
 				// get ClaimantClaimListActivity
 				ActivityMonitor activityMonitor = getInstrumentation().addMonitor(ClaimantClaimListActivity.class.getName(),null, false);
 				final ClaimantClaimListActivity clActivity = (ClaimantClaimListActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 10000);
-				
+					
 				// get claimListView
 				final ListView claimList = (ListView) clActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.claimListView);
 
 				// add new claim, position: index 0
 				cacc.addClaim("test", "2014-12-11", "2015-01-05");
-				
+					
 				// click new claim
 				assertNotNull(claimList.getChildAt(0).performClick());
-							
+								
 				/*
-				 * Test for US04.04.01 Basic Flow 5
+				 * Test for US06.01.01 Basic Flow 5
 				 */
-					
+						
 				// get ClaimantItemListActivity
 				ActivityMonitor activityMonitor1 = getInstrumentation().addMonitor(ClaimantItemListActivity.class.getName(),null, false);
 				final ClaimantItemListActivity ilActivity = (ClaimantItemListActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor1, 10000);
-					
+						
 				assertNotNull(ilActivity);
-					
+						
 				// get item list view
 				// view which is expected to be present on the screen
 				final View decorView3 = ilActivity.getWindow().getDecorView();
 				final ListView itemList = (ListView) ilActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.itemListView);	
 				// check if it is on screen
 				ViewAsserts.assertOnScreen(decorView3, itemList);
-				
+					
 				// check whether the Button object's width and height attributes match
 				// the expected values
 				final ViewGroup.LayoutParams layoutParams2 = clv.getLayoutParams();
@@ -170,19 +172,19 @@ public class US04_04_01_Test extends
 				assertEquals(layoutParams2.height,WindowManager.LayoutParams.WRAP_CONTENT);
 
 				/*
-				 * Test for US04.04.01 Basic Flow 6
+				 * Test for US06.01.01 Basic Flow 6
 				 */
-				
+					
 				final ListView itemList1 = (ListView) ilActivity.findViewById(ca.ualberta.CMPUT301W15T06.R.id.itemListView);
 				assertNotNull(itemList1.performClick());
 				ilActivity.runOnUiThread(new Runnable() {
 					@Override
 					public void run() {
-							
+								
 						// build controller
 						Claim claim = new Claim("test");
 						ClaimantAddItemController caic = new ClaimantAddItemController(claim);
-						
+							
 						// get ClaimantItemListActivity
 						ActivityMonitor activityMonitor1 = getInstrumentation().addMonitor(ClaimantItemListActivity.class.getName(),null, false);
 						final ClaimantItemListActivity ilActivity1 = (ClaimantItemListActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor1, 10000);
@@ -197,14 +199,14 @@ public class US04_04_01_Test extends
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
-						
+							
 						// click new item
 						assertNotNull(itemList1.getChildAt(0).performClick());
-						
+							
 						/*
-						 * Test for US04.04.01 Basic Flow 7
+						 * Test for US06.01.01 Basic Flow 7
 						 */
-						
+							
 						// get contextMenu
 						final GridView contextMenu = (GridView) ilActivity1.findViewById(ca.ualberta.CMPUT301W15T06.R.array.item_dialog_array);
 
@@ -212,21 +214,23 @@ public class US04_04_01_Test extends
 						assertNotNull(contextMenu.performClick());
 						
 						/*
-						 * Test for US04.04.01 Basic Flow 8
+						 * Test for US06.01.01 Basic Flow 8
 						 */
 						
 						ilActivity1.runOnUiThread(new Runnable(){
 
 							@Override
 							public void run() {
-								contextMenu.performItemClick(contextMenu, 0, claimList.getAdapter().getItemId(0));
+								contextMenu.performItemClick(contextMenu, 1, claimList.getAdapter().getItemId(0));
 							}
 						});
-								
-				
+					
 					}
 				});
 			}
 		});
 	}
+	
+	
+
 }
