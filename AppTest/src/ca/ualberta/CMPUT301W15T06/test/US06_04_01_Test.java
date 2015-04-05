633094b69af01c406167c695b8c0ecc64a69f555
xwu11 <czsharon1994@hotmail.com>
2015-04-05 05:17:50 -0600

    060401

---
+++ AppTest/src/ca/ualberta/CMPUT301W15T06/test/US06_04_01_Test.java
@@ -0,0 +1,7 @@
+//package ca.ualberta.CMPUT301W15T06.test;
+//
+//import android.test.ActivityInstrumentationTestCase2;
+//
+//public class US06_04_01_Test extends ActivityInstrumentationTestCase2<T> {
+//
+//}
