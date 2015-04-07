b6c5950e18e585bdfa7bc654abf2e57bf798a203
agileshaw <agileshaw@users.noreply.github.com>
2015-04-07 05:12:27 -0600

    Update ApproverClaimListController.java

--- App/src/ca/ualberta/CMPUT301W15T06/ApproverClaimListController.java
+++ App/src/ca/ualberta/CMPUT301W15T06/ApproverClaimListController.java
@@ -1,16 +1,42 @@
 package ca.ualberta.CMPUT301W15T06;
 
 import android.util.Log;
-
+/**
+ * <p>
+ * This <code>ApproverClaimListController</code> class controls the action of 
+ * approver for claimant. This controller class will displays a 
+ * list of <code>Claim</code>, add new comment to the <code>Claim</code>, 
+ * approve or return the <code>Claim</code>. It will be used when the 
+ * approver asks to view and change status to the <code>Claim</code>.
+ * <p>
+ * 
+ * @author CMPUT301W15T06
+ * @version 04/07/2015
+ * @see android.util.Log
+ */
 public class ApproverClaimListController {
 
+	/**
+	 * Set a <code>Claim</code> object claim which contains all the detail of a claim.
+	 */
 	private Claim claim;
 	
+	/**
+	 * Set a <code>Claim</code> to the claim list for approver.
+	 * 
+	 * @param claim  a claim in the claim list waiting for approver to view
+	 */
 	public ApproverClaimListController(Claim claim) {
 		// TODO Auto-generated constructor stub
 		this.claim=claim;
 	}
 
+	/**
+	 * Add a new String variable string as comment. Check network error and throws exceptions when necessary.
+	 * 
+	 * @param string  a String variable that approver entered as comment (like "Incomplete Claim Information")
+	 * @throws NetWorkException
+	 */
 	public void addComment(String string) throws NetWorkException {
 		// TODO Auto-generated method stub
 		Comment comment=new Comment(string);
@@ -27,6 +53,13 @@ public class ApproverClaimListController {
 		
 	}
 
+	/**
+	 * This method will allow approver to reutrn a claim after finish reviewing. 
+	 * Check for errors and throw exceptions when necessary. 
+	 * 
+	 * @throws NetWorkException
+	 * @throws WrongApproverException
+	 */
 	public void returnClaim() throws NetWorkException, WrongApproverException {
 		// TODO Auto-generated method stub
 
@@ -57,6 +90,13 @@ public class ApproverClaimListController {
 	
 	}
 
+	/**
+	 * This method will allow approver to approve a claim after finish reviewing. 
+	 * Check for errors and throw exceptions when necessary. 
+	 * 
+	 * @throws NetWorkException
+	 * @throws WrongApproverException
+	 */
 	public void approveClaim() throws NetWorkException, WrongApproverException {
 		// TODO Auto-generated method stub
 
